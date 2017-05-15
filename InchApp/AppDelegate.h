65e5c1ab9abb878f4b894daf2c0fb969d3088002
wade1987 <ucos_hqu@126.com>
2017-05-15 13:39:09 +0800

    Initial Commit

---
+++ InchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InchApp
+//
+//  Created by 张强 on 2017/5/15.
+//  Copyright © 2017年 Inch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InchApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InchApp
+//
+//  Created by 张强 on 2017/5/15.
+//  Copyright © 2017年 Inch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
