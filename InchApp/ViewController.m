a95e5e9587f2f8daf4ac840cc7b3eb337b1ee2a6
wade1987 <ucos_hqu@126.com>
2017-05-16 00:28:40 +0800

    增加启动界面

--- InchApp/ViewController.m
+++ InchApp/ViewController.m
@@ -12,11 +12,66 @@
 
 @end
 
+#define SCREEN_SIZE [UIScreen mainScreen].bounds.size
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //居中显示“INCH SIZER”
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(50, SCREEN_SIZE.height/4, SCREEN_SIZE.width-100, SCREEN_SIZE.height/3)];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"INCH SIZER";
+    label.textColor = [UIColor blackColor];
+    label.font = [UIFont fontWithName:@"Arial" size:45];
+    label.adjustsFontSizeToFitWidth =YES;
+    //label.font = [UIFont systemFontOfSize:42];
+    
+    //居中显示“量体衣”
+    UILabel *label3 = [[UILabel alloc]initWithFrame:CGRectMake(50, SCREEN_SIZE.height/4+100, SCREEN_SIZE.width-100, SCREEN_SIZE.height/3)];
+    label3.textAlignment = NSTextAlignmentCenter;
+    label3.text = @"量体衣";
+    label3.textColor = [UIColor blackColor];
+    label3.font = [UIFont fontWithName:@"Arial" size:45];
+    //label3.adjustsFontSizeToFitWidth =YES;
+    //label.font = [UIFont systemFontOfSize:42];
+    
+    //居中显示“Copyright ©️ 2017 Inch. All Rights Reserved"
+    UILabel *label1 = [[UILabel alloc]initWithFrame:CGRectMake(50, SCREEN_SIZE.height-50, SCREEN_SIZE.width-100, 50)];
+    label1.textAlignment = NSTextAlignmentCenter;
+    label1.text = @"Copyright 2017 Inch. All Rights Reserved";
+    label1.textColor = [UIColor blackColor];
+    label1.font = [UIFont systemFontOfSize:8];
+    
+    //右上角显示INCH LOGO 图片
+    UIImageView  *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(SCREEN_SIZE.width-32, 32, 32, 32)];
+    [imageView setImage:[UIImage imageNamed:@"InchLogo"]];
+    
+    //居中显示“www.inchplus.cn"
+    UILabel *label2 = [[UILabel alloc]initWithFrame:CGRectMake(50, SCREEN_SIZE.height-25, SCREEN_SIZE.width-100, 25)];
+    label2.textAlignment = NSTextAlignmentCenter;
+    label2.text = @"www.inchplus.cn";
+    label2.textColor = [UIColor blackColor];
+    label2.font = [UIFont systemFontOfSize:8];
+    
+    [self.view addSubview:label];
+    [self.view addSubview:label1];
+    [self.view addSubview:label2];
+    [self.view addSubview:label3];
+    [self.view addSubview:imageView];
+    
+    
+    //使能一个3.5s one time 定时器
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:3.5 target:self selector:@selector(timerISR) userInfo:nil repeats:NO];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:UITrackingRunLoopMode];
+    
+    
+}
+
+-(void)timerISR
+{
+    self.view.backgroundColor = [UIColor greenColor];
 }
 
 
